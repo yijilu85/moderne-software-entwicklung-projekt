a272459c4dbdb17e5dd5bb12c9f0f2191610a971
yiji <yiji85@googlemail.com>
2024-11-26 13:12:15 +0100

    add create appointment func

--- patientcare-fe/src/api/appointmentController.ts
+++ patientcare-fe/src/api/appointmentController.ts
@@ -2,7 +2,7 @@ import axios from "./axiosConfig";
 import type { Appointment, Doctor, Patient } from "@/types/types";
 
 export const getAllAppointments = async () => {
-  const response = await axios.get(`/appointments/`);
+  const response = await axios.get(`/appointments`);
   return response.data as Appointment[];
 };
 
@@ -14,4 +14,9 @@ export const getAppointment = async (id: number) => {
 export const getAllAppointmentsForUser = async (id: number) => {
   const response = await axios.get(`/appointment/?userId=${id}`);
   return response.data as Appointment[];
+};
+
+export const createAppointmentSlot = async (payload: Appointment) => {
+  const response = await axios.post(`/appointment`, payload);
+  return response.data as Appointment;
 };
