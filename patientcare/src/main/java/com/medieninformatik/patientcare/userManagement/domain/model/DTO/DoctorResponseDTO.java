b7f9319726494f8d6fef97af0edcbef5410bc0e6
yiji <yiji85@googlemail.com>
2024-11-28 18:56:18 +0100

    fix json creator doctorresponsedto

--- patientcare/src/main/java/com/medieninformatik/patientcare/userManagement/domain/model/DTO/DoctorResponseDTO.java
+++ patientcare/src/main/java/com/medieninformatik/patientcare/userManagement/domain/model/DTO/DoctorResponseDTO.java
@@ -4,6 +4,7 @@ import com.medieninformatik.patientcare.userManagement.domain.model.Doctor;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 @Getter
 @AllArgsConstructor
@@ -24,6 +25,7 @@ public class DoctorResponseDTO{
     private String licenseId;
     private String title;
 
+    @JsonCreator
     public DoctorResponseDTO(Doctor doctor) {
         this.userType = doctor.getUserType().toString();
         this.id = doctor.getId();
